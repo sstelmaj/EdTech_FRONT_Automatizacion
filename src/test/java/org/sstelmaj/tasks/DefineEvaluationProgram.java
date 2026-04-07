3f168599cd26a89169c5d7b820b2617dab6ae2be
sstelmaj <seba.stelmaj21@gmail.com>
2026-04-07 17:09:29 -0300

    feat(tasks): add define and edit evaluation program tasks

---
+++ src/test/java/org/sstelmaj/tasks/DefineEvaluationProgram.java
@@ -0,0 +1,64 @@
+package org.sstelmaj.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Clear;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import org.sstelmaj.interactions.WaitForMilliseconds;
+import org.sstelmaj.ui.CourseDetailPage;
+import org.sstelmaj.ui.EvaluationProgramModal;
+
+import java.util.List;
+import java.util.Map;
+
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
+
+public class DefineEvaluationProgram implements Task {
+
+    private final List<Map<String, String>> activities;
+
+    public DefineEvaluationProgram(List<Map<String, String>> activities) {
+        this.activities = activities;
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                WaitUntil.the(CourseDetailPage.DEFINE_PROGRAM_BUTTON, isVisible()).forNoMoreThan(10).seconds(),
+                Click.on(CourseDetailPage.DEFINE_PROGRAM_BUTTON),
+                WaitUntil.the(EvaluationProgramModal.TITLE, isVisible()).forNoMoreThan(10).seconds()
+        );
+
+        for (int i = 0; i < activities.size(); i++) {
+            int index = i + 1;
+            Map<String, String> activity = activities.get(i);
+
+            if (i > 0) {
+                actor.attemptsTo(
+                        Click.on(EvaluationProgramModal.ADD_ACTIVITY_BUTTON)
+                );
+                actor.attemptsTo(WaitForMilliseconds.of(500));
+            }
+
+            actor.attemptsTo(
+                    Clear.field(EvaluationProgramModal.activityNameFieldByIndex(index)),
+                    Enter.theValue(activity.get("nombre")).into(EvaluationProgramModal.activityNameFieldByIndex(index)),
+                    Clear.field(EvaluationProgramModal.activityPercentageFieldByIndex(index)),
+                    Enter.theValue(activity.get("porcentaje")).into(EvaluationProgramModal.activityPercentageFieldByIndex(index))
+            );
+        }
+
+        actor.attemptsTo(
+                Click.on(EvaluationProgramModal.SAVE_BUTTON),
+                WaitUntil.the(EvaluationProgramModal.SUCCESS_ALERT, isVisible()).forNoMoreThan(10).seconds(),
+                WaitForMilliseconds.of(1500)
+        );
+    }
+
+    public static DefineEvaluationProgram withActivities(List<Map<String, String>> activities) {
+        return Tasks.instrumented(DefineEvaluationProgram.class, activities);
+    }
+}
