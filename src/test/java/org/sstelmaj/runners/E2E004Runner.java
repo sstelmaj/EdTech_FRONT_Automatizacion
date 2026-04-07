3163107388b1f2d467103cc1cf930c32f3f8e3cb
sstelmaj <seba.stelmaj21@gmail.com>
2026-04-07 17:08:53 -0300

    feat(runners): add cucumber suite runners for all scenarios and individual e2e tags

---
+++ src/test/java/org/sstelmaj/runners/E2E004Runner.java
@@ -0,0 +1,17 @@
+package org.sstelmaj.runners;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "net.serenitybdd.cucumber.core.plugin.SerenityReporterParallel,pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.sstelmaj.stepdefinitions")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@E2E-004")
+public class E2E004Runner {
+}
